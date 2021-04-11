6cb98c7992ddf43623f8e4faf19759bcb6cfcdf6
Madhuja Mitra <madhujamitra3117@gmail.com>
2021-04-12 00:07:57 +0530

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+
 import { LoginComponent } from './login/login.component';
 import { AppCommentComponent } from './comment/app.component';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
@@ -12,18 +12,18 @@ import { RouterModule } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
-import { QueryComponent } from './query/query.component';
+
 import { CommentTree } from "./comment/comments/comment-tree.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
+ 
     LoginComponent,
     AppCommentComponent,
     BsNavbarComponent,
     RegisterComponent,
-    QueryComponent,
+    
     CommentTree
   ],
   imports: [
@@ -36,7 +36,7 @@ import { CommentTree } from "./comment/comments/comment-tree.component";
     AppRoutingModule,
     RouterModule.forRoot(
       [
-        { path: ' ', component: HomeComponent },
+        
         { path: 'login', component: LoginComponent },
         { path: 'comments', component: AppCommentComponent },
         { path: 'sign-up', component: RegisterComponent },
